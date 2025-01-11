8760e8b016c337b744ac6c9f2ec34c06a59cae37
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2025-01-11 14:07:17 +0700

    mhs admin

--- resources/views/pages/mahasiswa/index.blade.php
+++ resources/views/pages/mahasiswa/index.blade.php
@@ -29,10 +29,6 @@
                 <h2 class="text-lg font-bold text-navy dark:text-gray-100">
                     {{ __('Data Mahasiswa') }}
                 </h2>
-                <a href="{{ route('kriteria.create') }}"
-                    class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-success text-white hover:bg-successHover focus:outline-none focus:bg-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-neutral-800">
-                    {{ __('Tambah Data') }}
-                </a>
             </div>
         </div>
 
@@ -116,23 +112,61 @@
                                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
                                     aria-labelledby="dropdownMenuIconHorizontalButton">
                                     <li>
-                                        <a href="{{ route('kriteria.edit', $item->id) }}"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            onclick="event.preventDefault(); document.getElementById('delete-form-{{ $item->id }}').submit();"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hapus</a>
+                                        <a href="#" id="readProductButton-{{ $item->id }} }}"
+                                            data-modal-target="readProductModal-{{ $item->id }}"
+                                            data-modal-toggle="readProductModal-{{ $item->id }}"
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            Lihat Detail
+                                        </a>
                                     </li>
                                 </ul>
+
                             </div>
 
-                            <form id="delete-form-{{ $item->id }}"
-                                action="{{ route('kriteria.destroy', $item->id) }}" method="POST"
-                                style="display: none;">
-                                @csrf
-                                @method('DELETE')
-                            </form>
+                            <!-- Main modal -->
+                            <div id="readProductModal-{{ $item->id }}" tabindex="-1" aria-hidden="true"
+                                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
+                                    <!-- Modal content -->
+                                    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                                        <!-- Modal header -->
+                                        <div
+                                            class="flex justify-between mb-4 border-b border-gray-200 rounded-t sm:mb-5">
+                                            <div class="text-lg text-gray-900 md:text-xl dark:text-white">
+                                                <h3 class="font-semibold ">
+                                                    {{ $item->name }}
+                                                </h3>
+                                                <p class="font-bold">
+                                                    {{ $item->nim }}
+                                                </p>
+                                            </div>
+                                            <div>
+                                                <button type="button"
+                                                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    data-modal-toggle="readProductModal-{{ $item->id }}">
+                                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
+                                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fill-rule="evenodd"
+                                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd"></path>
+                                                    </svg>
+                                                    <span class="sr-only">Close modal</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <dl>
+                                            <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">
+                                                Email Mahasiswa</dt>
+                                            <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
+                                                {{ $item->email }}</dd>
+                                            <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">
+                                                Email Verifikasi</dt>
+                                            <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
+                                                {{ $item->email_verified_at->format('d M y - H:i:s') }}</dd>
+                                        </dl>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
